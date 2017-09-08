8f5f06dc4350911c55c0682ade85e642a33ff6eb
Kazuki Tobita <kazukiti201@gmail.com>
2017-09-08 13:28:12 +0900

    デザインの変更

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -3,3 +3,10 @@
         <router-view></router-view>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    * {
+        padding: 0;
+        width: 100%;
+    }
+</style>
--- resources/assets/js/pages/Capture.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div id="page">
-        <h1>test</h1>
-    </div>
-</template>
